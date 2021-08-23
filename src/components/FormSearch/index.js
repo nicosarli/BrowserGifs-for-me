c99b25a597e0114f779eb5abf58c542390fa9e40
nicosarli <nico_sarli@hotmail.com>
2021-08-23 17:54:33 -0300

    arreglando error del input

--- src/components/FormSearch/index.js
+++ src/components/FormSearch/index.js
@@ -50,57 +50,58 @@ const FormSearch = () => {
 
   return (
     <div className={classes.form}>
-  <FormControl onSubmit={handleSubmit} >
-    <TextField
-        className={classes.textFieldFrom}
-        id="outlined-basic"
-        label="Write a gif to browse"
-        variant="outlined"
-        size="small"
-        autoComplete="off"
-        value={keyword}
-        onChange={handleChange}
-        />
-  </FormControl>
-    <FormControl onSubmit={handleSubmit}>
-      <InputLabel id="ratio-of-gif" className={classes.FormControlLabel}> Select Ratio />
-        <Select 
-          className={classes.FormControlInput}
-          labelId="ratio-of-gif"
-          id="ratio-of-gif-select"
-          value={rating}
-          onChange={handleChangeRating}
-         >
-        <MenuItem value='g'> g </MenuItem>
-        <MenuItem value='pg'> pg </MenuItem>
-        <MenuItem value='pg-13'> pg-13 </MenuItem>
-        <MenuItem value='r'> r </MenuItem>
-        </Select>
-    </FormControl>
-    <FormControl  onSubmit={handleSubmit} >
-      <InputLabel id="language-of-gif" className={classes.FormControlLabel}> Select Language />
-        <Select 
-          className={classes.FormControlInput}
-          labelId="language-of-gif"
-          id="language-of-gif-select"
-          value={language}
-          onChange={handleChangeLanguage}
-         >
-        <MenuItem value='en'> en </MenuItem>
-        <MenuItem value='es'> es </MenuItem>
-        <MenuItem value='pt'> pt </MenuItem>
-        <MenuItem value='fr'> fr </MenuItem>
-        <MenuItem value='id'> id </MenuItem>
-        <MenuItem value='ar'> ar </MenuItem>
-        <MenuItem value='th'> th </MenuItem>
-        <MenuItem value='tr'> tr </MenuItem>
-        <MenuItem value='ja'> ja </MenuItem>
-        </Select>
+      <FormControl onSubmit={handleSubmit} >
+        <TextField
+            className={classes.textFieldFrom}
+            id="outlined-basic"
+            label="Write a gif to browse"
+            variant="outlined"
+            size="small"
+            autoComplete="off"
+            value={keyword}
+            onChange={handleChange}
+            />
+      </FormControl>
 
-  </FormControl>
-  <Button color="secondary" variant="contained" onClick={handleSubmit}>
-        Search a gifs...
-      </Button>
+        <FormControl onSubmit={handleSubmit}>
+          <InputLabel id="ratio-of-gif" className={classes.FormControlLabel}> Select Ratio </InputLabel>
+            <Select 
+              className={classes.FormControlInput}
+              labelId="ratio-of-gif"
+              id="ratio-of-gif-select"
+              value={rating}
+              onChange={handleChangeRating}
+            >
+            <MenuItem value='g'> g </MenuItem>
+            <MenuItem value='pg'> pg </MenuItem>
+            <MenuItem value='pg-13'> pg-13 </MenuItem>
+            <MenuItem value='r'> r </MenuItem>
+            </Select>
+        </FormControl>
+
+        <FormControl  onSubmit={handleSubmit} >
+          <InputLabel id="language-of-gif" className={classes.FormControlLabel}> Select Language </InputLabel>
+              <Select 
+                className={classes.FormControlInput}
+                labelId="language-of-gif"
+                id="language-of-gif-select"
+                value={language}
+                onChange={handleChangeLanguage}
+              >
+              <MenuItem value='en'> en </MenuItem>
+              <MenuItem value='es'> es </MenuItem>
+              <MenuItem value='pt'> pt </MenuItem>
+              <MenuItem value='fr'> fr </MenuItem>
+              <MenuItem value='id'> id </MenuItem>
+              <MenuItem value='ar'> ar </MenuItem>
+              <MenuItem value='th'> th </MenuItem>
+              <MenuItem value='tr'> tr </MenuItem>
+              <MenuItem value='ja'> ja </MenuItem>
+              </Select>
+        </FormControl>
+          <Button color="secondary" variant="contained" onClick={handleSubmit}>
+            Search a gifs...
+          </Button>
   </div>
   );
 };
